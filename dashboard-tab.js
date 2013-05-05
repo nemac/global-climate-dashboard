987a2770fe5d6b079491d79aa512bdf13a555e12
Mark Phillips <embeepea@git>
2013-05-05 14:01:38 -0400

    make graph buttons do the right thing

--- dashboard-tab.js
+++ dashboard-tab.js
@@ -13,50 +13,118 @@
             + '</div>'
     );
 
+    var graphSlotTpl = (
+        ''
+            +   '<div class="dashboard-tab-graphslot"/>'
+    );
+
     var methods = {
         init : function(options) {
             return this.each(function() {
-                function setButtonSelected(button) {
-                    $.each(graphButtonList, function() {
-                        $(this).removeClass("dashboard-button-selected");
-                    });
-                    $(button).addClass("dashboard-button-selected");
+                var $this                   = $(this),
+                    data                    = $this.data('dashboard_tab'),
+                    $firstDashboardGraphDiv = undefined,
+                    graphButtonList         = [],
+                    graphSlotList           = [],
+                    graphList               = [],
+                    $graphSlotsDiv,
+                    $timeRegionDiv,
+                    $graphButtonsDiv,
+                    n,
+                    settings = $.extend({
+                        numSlots : 3
+                    }, options);
+
+                function displayGraph(i) {
+                    // Display the i-th graph in this tab's first slot, moving any other graphs
+                    // down one slot each, with the last graph being removed from any slot entirely,
+                    // and updating the states of all the graph buttons to show the currently
+                    // displayed graphs.
+
+                    var j, prevGraphIndex, currentGraphIndex;
+
+                    //
+                    // If this graph is already in a slot, do nothing
+                    //
+                    for (j=0; j<settings.numSlots; ++j) {
+                        if (graphSlotList[j].data('dashboard-graph-index') === i) {
+                            return;
+                        }
+                    }
+
+                    //
+                    // get the index of the graph that is going to become undisplayed,
+                    // so that we can alter the state of its button below
+                    //
+                    var removedGraphIndex = undefined;
+                    var lastSlot = graphSlotList[settings.numSlots-1];
+                    if ( (lastSlot !== undefined) &&
+                         (lastSlot.data('dashboard-graph-index') !== undefined) ) {
+                        removedGraphIndex = lastSlot.data('dashboard-graph-index');
+                    }
+
+                    //
+                    // For each slot except the first, move the graph above it down into it.
+                    // This results in the graph that was in the last slot no longer being displayed.
+                    //
+                    for (j=settings.numSlots-1; j>0; --j) {
+                        prevGraphIndex    = graphSlotList[j-1].data('dashboard-graph-index');
+                        if (prevGraphIndex !== undefined) {
+                            graphSlotList[j].empty().append(graphList[prevGraphIndex]);
+                            graphSlotList[j].data('dashboard-graph-index', prevGraphIndex);
+                        }
+                    }
+
+                    //
+                    // Put the given graph into the first slot
+                    //
+                    graphSlotList[0].empty().append(graphList[i]);
+                    graphSlotList[0].data('dashboard-graph-index', i);
+
+                    //
+                    // update button states
+                    //
+                    if (removedGraphIndex !== undefined) {
+                        graphButtonList[removedGraphIndex].removeClass("dashboard-button-selected");
+                    }
+                    graphButtonList[i].addClass("dashboard-button-selected");
+
                 }
 
-                var $this = $(this),
-                    data = $this.data('dashboard_tab'),
-                    settings = $.extend({
-                    }, options);
                 if ( ! data ) {
                     $this.html(Mustache.to_html(dashboardTabTpl, {
                     }));
-                    var $graphSlots = $this.find(".dashboard-tab-graphslots");
-                    var $timeRegion = $this.find(".dashboard-tab-timeregion");
-                    var $graphButtons = $this.find(".dashboard-tab-graphbuttons");
 
-                    var firstDashboardGraphDiv = undefined;
+                    $graphSlotsDiv = $this.find(".dashboard-tab-graphslots");
+                    $timeRegionDiv = $this.find(".dashboard-tab-timeregion");
+                    $graphButtonsDiv = $this.find(".dashboard-tab-graphbuttons");
 
-                    var graphButtonList = [];
 
                     //
-                    // add 3 graphs
+                    // add the requested number of graph slots
                     //
-                    var N = 0;
-                    $.each(settings.graphs, function () {
+                    for (n=0; n<settings.numSlots; ++n) {
+                        graphSlotList.push($('<div class="dashboard-tab-graphslot"/>').appendTo($graphSlotsDiv));
+                    }
+
+                    //
+                    // create the graphs and graph buttons
+                    //
+                    $.each(settings.graphs, function (i) {
                         var graph = this;
-                        if (N++ < 3) {
-                            var dashboardGraphDiv = $('<div/>').dashboard_graph({
-                                title       : graph.title,
-                                description : graph.description,
-                                error       : function (e) { throw e; },
-                                warning     : function (e) { console.log(e); },
-                                width       : 560,
-                                height      : 104,
-                                muglString  : graph.mugl
-                            }).appendTo($graphSlots);
-                            if (firstDashboardGraphDiv === undefined) {
-                                firstDashboardGraphDiv = dashboardGraphDiv;
-                            }
+
+                        var $graphDiv = $('<div/>').dashboard_graph({
+                            title       : graph.title,
+                            description : graph.description,
+                            error       : function (e) { throw e; },
+                            warning     : function (e) { console.log(e); },
+                            width       : 560,
+                            height      : 104,
+                            muglString  : graph.mugl
+                        });
+                        graphList.push($graphDiv);
+                        if ($firstDashboardGraphDiv === undefined) {
+                            $firstDashboardGraphDiv = $graphDiv;
                         }
                         graphButtonList.push(
                             $('<div class="dashboard-graph-button"/>')
@@ -65,25 +133,35 @@
                                 })
                                 .data('dashboard-button-state', 'unpressed')
                                 .click(function() {
-                                    console.log(graph.shortTitle + ' button clicked; state is: ' +
-                                                $(this).data('dashboard-button-state'));
-                                    setButtonSelected(this);
+                                    /*
+                                     console.log(graph.shortTitle + ' button clicked; state is: ' +
+                                     $(this).data('dashboard-button-state'));
+                                     */
+                                    displayGraph(i);
                                 })
-                                .appendTo($graphButtons)
+                                .appendTo($graphButtonsDiv)
                         );
                     });
 
                     //
+                    // fill the available graph slots with the first graphs from the list
+                    //
+                    for (n=settings.numSlots-1; n>=0; --n) {
+                        displayGraph(n);
+                    }
+
+
+                    //
                     // add a timeline
                     //
-                    $timeRegion.append($('<div/>').dashboard_timeline({
+                    $timeRegionDiv.append($('<div/>').dashboard_timeline({
                         error       : function (e) { throw e; },
                         warning     : function (e) { console.log(e); },
                         width       : 560,
                         height      : 20,
                         muglString  : settings.timelineMugl
                     }));
-                    firstDashboardGraphDiv.dashboard_graph('multigraphDone', function (multigraph) {
+                    $firstDashboardGraphDiv.dashboard_graph('multigraphDone', function (multigraph) {
                         var axis = multigraph.graphs().at(0).axes().at(0);
                         var sliderActive = false;
                         var yearFormatter = new window.multigraph.core.DatetimeFormatter("%Y");
@@ -115,7 +193,7 @@
                                 multigraph.redraw();
                                 sliderActive = false;
                             }
-                        }).appendTo($timeRegion);
+                        }).appendTo($timeRegionDiv);
                     });
 
                     $this.data('dashboard_tab', {
